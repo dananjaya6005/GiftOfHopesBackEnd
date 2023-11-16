ec46e523d42a4684b361eb2d1b431f74f34c757f
dananjaya6005 <dananjaya6005@gmail.com>
2023-11-17 00:04:48 +0530

    init

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_51OCjmQEnnlTCDnlQHJqXqcrcsZhBAGQf1OTy2S0hg5LLLoVy1evnXTMUEmCZ6OGm4O3JRoA8WCPDSSRGpxZyuvsw00KqXboqUx');
+const app = express();
+const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
+
+app.use(cors());
+
+const supabase = createClient(
+  "https://pjqbnzerwqygskkretxd.supabase.co",
+
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqcWJuemVyd3F5Z3Nra3JldHhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk2NzQzNzYsImV4cCI6MjAxNTI1MDM3Nn0.tCOsaglHK0BVL689wrFKvQIVat88jhKow-yMsURlJSE"
+);
+
+
+
+app.get('/afterpayment', async (req, res) => {
+  const sessionId = req.query.checkout_session_id;
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  const clientReferenceId = session.client_reference_id;
+  const amount = session.amount_total;
+  const custom_fields = session.custom_fields[0].numeric.value;
+  console.log(custom_fields);
+  console.log(clientReferenceId);
+  console.log(amount);
+ 
+  // Store the client reference ID and amount in supabase database
+  // ...
+
+ 
+const { data, error } = await supabase
+.from('Payment_info')
+.insert([
+  { donation_id: custom_fields,client:clientReferenceId, amount: amount },
+])
+.select()
+        
+  console.log(data, error);
+
+     // Send an HTML response
+     res.send(`
+     <html>
+     <head>
+       <title>Payment Success</title>
+       <style>
+         body {
+           font-family: 'Arial', sans-serif;
+           text-align: center;
+           background-color: #f0f0f0;
+         }
+         h2 {
+           color: #008000; /* Green color */
+         }
+       </style>
+     </head>
+     <body>
+       <h2>Payment information successfully stored.</h2>
+       <br />
+       <br />
+        <a href="http://localhost:5173/">Go back to the homepage</a>
+
+     </body>
+   </html>
+   `);
+ 
+});
+
+app.listen(3000, () => console.log('Node server listening on port 3000!'));
